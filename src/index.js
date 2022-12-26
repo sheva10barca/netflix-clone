a538a2d70284bb0309ef37a101c36e2bb36caf06
sheva <vladislavshevchenko10@gmail.com>
2022-12-26 22:07:41 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import store from './app/store';
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-   <Provider store={store}>
-      <App />
-   </Provider>,
+   <HashRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
